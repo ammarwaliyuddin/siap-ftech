95a13fad634faa46337bbd78c24ef266502fb2a1
ammar.w <ammarwaliyuddin@gmail.com>
2022-11-22 09:36:47 +0700

    add cover

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -45,15 +45,17 @@
     <!-- Page CSS -->
     <!-- Page -->
     <link rel="stylesheet" href="<?= base_url('assets/vendor/css/pages/page-auth.css') ?>" />
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>" />
     <!-- Helpers -->
     <script src="<?= base_url('assets/vendor/js/helpers.js') ?>"></script>
-
+    
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?= base_url('assets/js/config.js') ?>"></script>
+      <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+      <script src="<?= base_url('assets/js/config.js') ?>"></script>
   </head>
 
-  <body>
+  <body class="body">
     <!-- Content -->
 
     <div class="container-xxl">
